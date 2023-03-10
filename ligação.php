7ea91dd55206614d5245790cb33ba2d41d410d95
Matheus Polato Benatte Fernandes <documentario2sesi@gmail.com>
2023-03-10 11:29:13 -0300

    fazendo

--- ligação.php
+++ ligação.php
@@ -45,6 +45,9 @@
 <main class="container">
   <div class="bg-light p-5 rounded">
     <h1>Usuários</h1>
+    <?php
+    $sql="select count (id) "
+    ?>
     <p class="lead">This example is a quick exercise to illustrate how the top-aligned navbar works. As you scroll, this navbar remains in its original position and moves with the rest of the page.</p>
     <a class="btn btn-lg btn-primary" href="usuario/TelaListagem.php" role="button">Lista de Usuários &raquo;</a>
   </div>
